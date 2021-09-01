50189794489127bd816a5a3a1b13e464720ae911
sheikhemon <sheikhemon@gmail.com>
2021-09-01 12:22:15 +0600

    added callback

--- calback.js
+++ calback.js
@@ -1,7 +1,23 @@
-function callback (name) {
-    console.log(name);
+function welcomeMessage (name, greetHandler) {
+    greetHandler(name);
 }
-function morning() {
-    console.log('good morning');
+function greetMorning (name) {
+    console.log('good morning', name);
 }
-callback('aiman')
+function greetAfternoon (name) {
+    console.log('good Afternoon', name);
+}
+function greetEvening (name) {
+    console.log('good morning', name);
+}
+welcomeMessage("Tom hanks", greetMorning)
+welcomeMessage("Tom Crouse", greetAfternoon)
+welcomeMessage("Tom Barlee", greetEvening)
+
+
+const names = 'Tom Hanks';
+welcomeMessage(names)
+const namesArr = ['Tom Hanks', 'Tom Crouse', 'Tom Barlee']
+welcomeMessage(namesArr)
+const myObj = {name: 'Emon', Id: 45}
+welcomeMessage(myObj)
